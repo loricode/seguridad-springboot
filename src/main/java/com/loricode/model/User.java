5027b9e9462425a80b78ca88d87ead62a9e18dea
campilloajc <ajcampillofsi@gmail.com>
2023-01-13 15:42:59 -0500

    first commit

---
+++ src/main/java/com/loricode/model/User.java
@@ -0,0 +1,47 @@
+package com.loricode.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@Id
+	@Column(length = 36)
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy="org.hibernate.id.UUIDGenerator")
+	private String id;
+	
+	@Column(name="fullname", length = 180)
+	private String fullName;
+	
+	@Column(name="email", length = 180)
+	private String email;
+
+	public User(String id, String fullName, String email) {
+		this.id = id;
+		this.fullName = fullName;
+		this.email = email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	
+}
